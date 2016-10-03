d750e992158d7c4ff882b65a54d53adf743abd10
Ítalo Vieira <italogv@hotmail.com>
2016-10-03 13:57:12 -0300

    feature: List removed guests
    Fix #1.

--- lua/mini-projeto-1-lua.lua
+++ lua/mini-projeto-1-lua.lua
@@ -1,27 +1,26 @@
 -- Require: https://github.com/harningt/luajson
 local json = require('json')
 
-file = 'guests.json' -- File to export data
-
-local load_data = function ()
+-- Load data from JSON file to Lua's table
+local load_data = function (file)
     local f = io.open(file, 'r')
     if f == nil then
-        guests = {}
+        return {}
     else
         local content = f:read('*all')
-        guests = json.decode(content) -- Decode file to Lua's object table
         f:close()
+        return json.decode(content) -- Decode file to Lua's object table
     end
 end
 
-local ls = function ()
+local ls = function (guests)
     if next(guests) == nil then
         print "It's empty."
     else
         for i, guest in ipairs(guests) do
             print(i .. ':')
             for k, v in pairs(guest) do
-                print('  ' .. k, v)
+                print('  ' .. k .. ': ' .. v)
             end
         end
     end
@@ -38,8 +37,8 @@ local search = function (str)
         for i, guest in ipairs(guests) do
             if string.find(guest.cpf, str) then
                 print(i .. ':')
-                print('  ' .. 'name', guest.name)
-                print('  ' .. 'cpf', guest.cpf)
+                print('  ' .. 'name:' .. guest.name)
+                print('  ' .. 'cpf:'.. guest.cpf)
             else
                 print "No results found."
             end
@@ -47,25 +46,32 @@ local search = function (str)
     end
 end
 
-local rm = function (id)
-    if guests[id] == nil then
+local rm = function (id, reason)
+    local guest = guests[id]
+    if guest == nil then
         print('The guest of id ' .. id .. ' does not exist.')
     else
         table.remove(guests, id)
+        guest.reason = reason
+        table.insert(removed_guests, guest)
     end
 end
 
-local save = function ()
-    if next(guests) ~= nil then
+local save = function (table, file)
+    if next(table) ~= nil then
         local f = io.open(file, 'w')
-        f:write(json.encode(guests))
+        f:write(json.encode(table))
         f:close()
     end
 end
 
 --
 
-load_data()
+local file = 'guests.json'
+local file_removed_guests = 'removed.json'
+
+guests = load_data(file)
+removed_guests = load_data(file_removed_guests)
 
 print "1: list"
 print "2: add"
@@ -79,7 +85,17 @@ while true do
     local o = io.read()
 
     if o == '1' or string.match('list', '^' .. o) then
-        ls()
+        print('1: guests')
+        print('2: removed guests')
+        io.write('What now% ')
+        local o = io.read()
+        if o == '1' or string.match('guests', '^' .. o) then
+            ls(guests)
+        elseif o == '2' or string.match('removed guests', '^' .. o) then
+            ls(removed_guests)
+        else
+            io.stderr:write("Invalid option\n")
+        end
     elseif o == '2' or string.match('add', '^' .. o) then
         io.write('Name: ')
         name = io.read()
@@ -91,16 +107,22 @@ while true do
         str = io.read()
         search(str)
     elseif o == '4' or string.match('remove', '^' .. o) then
-        io.write('Remove by id: ')
-        id = io.read()
-        rm(id)
+        local id
+        repeat
+            io.write('Remove by id: ')
+            id = tonumber(io.read())
+        until id ~= nil
+        io.write('Reason: ')
+        local reason = io.read()
+        rm(id, reason)
     elseif o == '5' or string.match('help', '^' .. o) then
         print "list   - list party guests"
         print "add    - add party guests"
         print "search - search party guests by CPF"
         print "remove - remove a party guests by id"
     elseif o == '6' or string.match('quit', '^' .. o) then
-        save()
+        save(guests, file)
+        save(removed_guests, file_removed_guests)
         print 'Bye.'
         os.exit()
     else
